0cf1488ad2c3c7c3eb41e1a5789f7e1319263870
VaseemAkram <vaseemakram07271@gmail.com>
2023-10-24 15:06:37 +0530

    file-added

---
+++ src/Conn.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "W@2915djkq#");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
